e0191497228d0057bf2f417c31e87f3b5ee445d7
PHUICMT <PHUICMT@github.com>
2021-08-23 10:03:24 +0700

    📝 Edit sql file

--- db/phq-9-db.sql
+++ db/phq-9-db.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `questionnaire` (
-  `id` varchar(255) PRIMARY KEY AUTO_INCREMENT,
+  `id` varchar(255) PRIMARY KEY,
   `created_at` timestamp
 );
 
 CREATE TABLE `videos` (
-  `id` varchar(255) PRIMARY KEY,
-  `questionnaire_id` varchar(255),
+  `id` varchar(255),
   `video_name` varchar(255),
+  `questionnaire_id` varchar(255),
   `video_type_is_webcam` boolean,
-  `file` blob
+  PRIMARY KEY (`id`, `video_name`)
 );
 
 CREATE TABLE `result` (
@@ -19,3 +19,4 @@ CREATE TABLE `result` (
 ALTER TABLE `videos` ADD FOREIGN KEY (`questionnaire_id`) REFERENCES `questionnaire` (`id`);
 
 ALTER TABLE `result` ADD FOREIGN KEY (`questionnaire_id`) REFERENCES `questionnaire` (`id`);
+
